c4c2812718fcb7efb24b86c93d4e8d9a991d9ac1
Collin McIntyre <crm11396@gmail.com>
2023-10-18 18:00:12 -0500

    day 4 code

---
+++ src/main/java/frc/robot/subsystems/PIDDreidel.java
@@ -0,0 +1,61 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj.ADXRS450_Gyro;
+import edu.wpi.first.wpilibj.AnalogInput;
+import edu.wpi.first.wpilibj.motorcontrol.VictorSP;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class PIDDreidel extends SubsystemBase
+{
+    // Gyro
+    // Motor Controller
+    // Potentiometer
+    // PID Controller
+    ADXRS450_Gyro gyro;
+    VictorSP motor;
+    AnalogInput pot;
+    PIDController pid;
+
+    public PIDDreidel()
+    {
+        gyro = new ADXRS450_Gyro();
+        motor = new VictorSP(4);
+        pot = new AnalogInput(0);
+        pid = new PIDController(0.01, 0, 0);
+
+        pot.setAverageBits(2);
+        pot.setOversampleBits(0);
+
+        pid.enableContinuousInput(0, 360);
+    }
+
+    public void spin()
+    {
+
+    }
+
+    public double getTarget()
+    {
+        return -getCurrentRobotRotation();
+    }
+
+    public double getCurrentRobotRotation()
+    {
+        return Math.IEEEremainder(-gyro.getAngle(), 360);
+    }
+
+    public double getCurrentPotRotation()
+    {
+        // y = m(x - x1) + y1
+
+        double x = pot.getAverageValue(); // x
+
+        // (x1, y1) = (3996, 360)
+        // (x2, y2) = (2, 0)
+
+        double m = 360.0 / 3994.0;
+
+        return m * (x - 2);
+    }
+}
